df7dcd63c2b06ffd361c278311be4bbaaf5fcfaa
Mateusz Miśkiewicz <br.miskiewicz@gmail.com>
2023-02-15 19:56:07 +0100

    add additional routes to create, update and delete cafe

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_ckeditor import CKEditor, CKEditorField
-
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, BooleanField, SelectField, FloatField
+from wtforms.validators import DataRequired, URL
 
 
 app = Flask(__name__)
@@ -30,6 +32,21 @@ class Cafe(db.Model):
     coffee_price = db.Column(db.String(250), nullable=False)
 
 
+class CreateCafeForm(FlaskForm):
+    name = StringField("Cafe Name", validators=[DataRequired()])
+    map_url = StringField("Google Maps URL", validators=[DataRequired(), URL()])
+    img_url = StringField("Image URL", validators=[DataRequired(), URL()])
+    location = StringField("Location", validators=[DataRequired()])
+    has_sockets = BooleanField("Has sockets")
+    has_toilet = BooleanField("Has toilet")
+    has_wifi = BooleanField("Has Wi-Fi")
+    can_take_calls = BooleanField("Can take calls")
+    seats = SelectField("seats", choices=[("0-10", "0-10"), ("10-20", "10-20"), ("20-30", "20-30"), ("30-40", "30-40"),
+                                          ("40-50", "40-50"), ("50+", "50+")])
+    coffee_price = StringField("Coffee Price", validators=[DataRequired()])
+    submit = SubmitField("Submit Cafe Entry")
+
+
 @app.route('/')
 def get_all_cafes():
     cafes = Cafe.query.all()
@@ -39,6 +56,69 @@ def get_all_cafes():
 def show_cafe(cafe_id):
     requested_cafe = Cafe.query.get(cafe_id)
     return render_template("cafe.html", cafe=requested_cafe)
+
+
+@app.route("/new-cafe", methods=["GET", "POST"])
+def add_new_cafe():
+    form = CreateCafeForm()
+    if form.validate_on_submit():
+        new_cafe = Cafe(
+            name=form.name.data,
+            map_url=form.map_url.data,
+            img_url=form.img_url.data,
+            location=form.location.data,
+            has_sockets=form.has_sockets.data,
+            has_toilet=form.has_toilet.data,
+            has_wifi=form.has_wifi.data,
+            can_take_calls=form.can_take_calls.data,
+            seats=form.seats.data,
+            coffee_price=form.coffee_price.data
+        )
+        db.session.add(new_cafe)
+        db.session.commit()
+        return redirect(url_for("get_all_cafes"))
+    return render_template("cafe-form.html", form=form)
+
+
+@app.route("/delete/<int:cafe_id>")
+def delete_cafe(cafe_id):
+    cafe_to_delete = Cafe.query.get(cafe_id)
+    db.session.delete(cafe_to_delete)
+    db.session.commit()
+    return redirect(url_for('get_all_cafes'))
+
+
+@app.route("/edit-cafe/<int:cafe_id>", methods=["GET", "POST"])
+def edit_cafe(cafe_id):
+    cafe = Cafe.query.get(cafe_id)
+
+    edit_form = CreateCafeForm(
+        name=cafe.name,
+        map_url=cafe.map_url,
+        img_url=cafe.img_url,
+        location=cafe.location,
+        has_sockets=cafe.has_sockets,
+        has_toilet=cafe.has_toilet,
+        has_wifi=cafe.has_wifi,
+        can_take_calls=cafe.can_take_calls,
+        seats=cafe.seats,
+        coffee_price=cafe.coffee_price
+    )
+    if edit_form.validate_on_submit():
+        cafe.name = edit_form.name.data
+        cafe.map_url = edit_form.map_url.data
+        cafe.img_url = edit_form.img_url.data
+        cafe.location = edit_form.location.data
+        cafe.has_sockets = edit_form.has_sockets.data
+        cafe.has_toilet = edit_form.has_toilet.data
+        cafe.has_wifi = edit_form.has_wifi.data
+        cafe.can_take_calls = edit_form.can_take_calls.data
+        cafe.seats = edit_form.seats.data
+        cafe.coffee_price = edit_form.coffee_price.data
+        db.session.commit()
+        return redirect(url_for("show_cafe", cafe_id=cafe.id))
+    return render_template("cafe-form.html", form=edit_form, is_edit=True)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
